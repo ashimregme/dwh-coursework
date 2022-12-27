777a9adb262c283fd64c883f54c3f804f9922e4f
Ashim Regmi <github.hawkmoth@aleeas.com>
2022-12-27 10:42:16 +0545

    removed unnecessary line

--- etl/Logger.py
+++ etl/Logger.py
@@ -15,7 +15,6 @@ class Logger:
         current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         log_file_name = str(v.get("SCRIPT_NAME")) + "_" + current_time + ".log"
         log_file_name = path.join(v.get("LOG_PATH"), log_file_name)
-        # log_file_name = v.get("LOG_FILE_NAME")
         log_file = open(log_file_name, 'w')
         log_file.write(self.message)
         log_file.close()
