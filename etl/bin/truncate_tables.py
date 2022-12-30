da6cfbe52d314a40f8c241ba47ce22e443527b3b
Ashim Regmi <github.hawkmoth@aleeas.com>
2022-12-30 22:34:48 +0545

    etl to load agg sales table complete

--- etl/bin/truncate_tables.py
+++ etl/bin/truncate_tables.py
@@ -32,15 +32,7 @@ def truncate():
             'LYLTY_CRD_T',
             'PDT_T',
             'CSTMR_T',
-            'PRMTN_SCHM',
-            'TIME_YEAR_T',
-            'TIME_HALF_YEAR_T',
-            'TIME_QUARTER_T',
-            'TIME_MONTH_T',
-            'TIME_DAY_T',
-            'TIME_HOUR_T',
-            'TIME_MIN_T',
-            'TIME_SEC_T'
+            'PRMTN_SCHM_T'
         ],
         "TARGET_F_BOSS": [
             'SLS_T',
